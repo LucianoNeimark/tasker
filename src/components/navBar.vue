b0d46984b769b76539410803a5eccb86e83d986a
Luciano <lucianoneimark@gmail.com>
2018-08-23 16:11:31 -0300

    Nav Bar

---
+++ src/components/navBar.vue
@@ -0,0 +1,26 @@
+<template>
+	<b-navbar toggleable="md" type="dark" variant="dark" sticky="true">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">Tasker</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Link1</b-nav-item>
+      <b-nav-item href="#">link2</b-nav-item>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+
+</template>
+
+<script>
+	
+
+</script>
+
+<style>
+</style>
